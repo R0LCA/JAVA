4d8e4dfcf06c8c2972e6a34842c24b3db5ece4b3
rolca <rolca999@gmail.com>
2024-10-03 18:48:48 +0200

    Update T3ejercicio16.java

--- t3ejercicio16/src/t3ejercicio16/T3ejercicio16.java
+++ t3ejercicio16/src/t3ejercicio16/T3ejercicio16.java
@@ -23,50 +23,42 @@ public class T3ejercicio16 {
     System.out.println("El desglose obtenido es...");
     if(euros>=500){
         int bill500 = euros/500;
-        System.out.println(bill500 + " billetes de 500");
+        System.out.println(bill500 + " billetes de 500€");
         euros = euros - bill500 * 500;
     }    
     if(euros>=200){
         int bill200 = euros/200;
-        System.out.println(bill200 + " billetes de 200");
+        System.out.println(bill200 + " billetes de 200€");
         euros = euros - bill200 * 200;
     }        
-            if(euros>=50){
-                int bill50 = euros/50;
-                System.out.println(bill50 + " billetes de 50");
-                euros = euros - bill50 * 50;
-                
-                if(euros>=20){
-                    int bill20 = euros/20;
-                    System.out.println(bill20 + " billetes de 20");
-                    euros = euros - bill20 * 20;
-                
-                    if(euros>=10){
-                        int bill10 = euros/10;
-                        System.out.println(bill10 + " billetes de 10");
-                        euros = euros - bill10 * 10;
-                        
-                        if(euros>=5){
-                            int bill5 = euros/5;
-                            System.out.println(bill5 + " billetes de 5");
-                            euros = euros - bill10 * 5;
-                            
-                            if(euros>=2){
-                                int mon2 = euros/2;
-                                System.out.println(mon2 + " billetes de 2");
-                                euros = euros - mon2 * 2;
-                                
-                                if(euros>=1){
-                                    int mon1 = euros/1;
-                                    System.out.println(mon1 + " billetes de 1");
-                                    euros = euros - mon1 * 1;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+    if(euros>=50){
+        int bill50 = euros/50;
+        System.out.println(bill50 + " billetes de 50€");
+        euros = euros - bill50 * 50;
+    }            
+    if(euros>=20){
+        int bill20 = euros/20;
+        System.out.println(bill20 + " billetes de 20€");
+        euros = euros - bill20 * 20;
+    }            
+    if(euros>=10){
+        int bill10 = euros/10;
+        System.out.println(bill10 + " billetes de 10€");
+        euros = euros - bill10 * 10;
+    }                    
+    if(euros>=5){
+        int bill5 = euros/5;
+        System.out.println(bill5 + " billetes de 5€");
+        euros = euros - bill5 * 5;
+    }                        
+    if(euros>=2){
+        int mon2 = euros/2;
+        System.out.println(mon2 + " monedas de 2€");
+        euros = euros - mon2 * 2;
+    }                            
+    if(euros>=1){
+        int mon1 = euros/1;
+        System.out.println(mon1 + " monedas de 1€");
     }
     
     
